5e7545eb9cb70e442b34cd9dc1538f82c54dfeee
vivek mutyala <vivekchowdarymutyala@outlook.com>
2023-12-02 21:01:43 +0530

    db script updated.

--- DbScript.sql
+++ DbScript.sql
@@ -6,3 +6,5 @@ AS
 SELECT U.*, UT.UserType UserTypeName, UT.[Description],UT.IsDeleted AS IsDeletedType FROM [DBO].[Users] U
 JOIN [DBO].UserType UT ON U.UserTypeId = UT.UserTypeId
 WHERE UserName = @UserName AND [Password] = @Password
+
+Go
