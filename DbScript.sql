1ba060006219b4a8defe2dbaa3581d5346b8a0ac
Suresh Kalaga <suresh.kalaga@outlook.com>
2023-12-02 20:50:41 +0530

    login sp added.

--- DbScript.sql
+++ DbScript.sql
@@ -1,1 +1,8 @@
 ﻿
+GO
+CREATE OR ALTER PROC [DBO].[Get_UserDetails] 
+(@UserName VARCHAR(50),@Password VARCHAR(50))
+AS
+SELECT U.*, UT.UserType UserTypeName, UT.[Description],UT.IsDeleted AS IsDeletedType FROM [DBO].[Users] U
+JOIN [DBO].UserType UT ON U.UserTypeId = UT.UserTypeId
+WHERE UserName = @UserName AND [Password] = @Password
