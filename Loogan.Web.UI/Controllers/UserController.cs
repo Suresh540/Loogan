19c87eb92cf56e20685ef017ab5c98a6426d373a
vivek mutyala <vivekchowdarymutyala@outlook.com>
2023-12-10 14:45:57 +0530

    Create User  in Admin

--- Loogan.Web.UI/Controllers/UserController.cs
+++ Loogan.Web.UI/Controllers/UserController.cs
@@ -20,6 +20,20 @@ namespace Loogan.Web.UI.Controllers
             return View();
         }
 
+        [Route("GetAllUser")]
+        public async Task<JsonResult> GetAllUser(UserModel user)
+        {
+            var userModel = await _utilityHelper.ExecuteAPICall<bool>(user, RestSharp.Method.Post, resource: "api/User/AllUser");
+            return Json(new { value = "Success" });
+        }
+
+        [Route("CreateUser")]
+        public async Task<JsonResult> CreateUser(UserModel user)
+        {
+            var userModel = await _utilityHelper.ExecuteAPICall<bool>(user, RestSharp.Method.Post, resource: "api/User/CreateUser");
+            return Json(new { value = "Success" });
+        }
+
         [Route("UpdateUser")]
         public async Task<JsonResult> UpdateUser(UserModel user)
         {
