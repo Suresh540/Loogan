b8a37d3c098050eba54b5dfa351292b5c8fef2db
Suresh Kalaga <suresh.kalaga@outlook.com>
2023-12-23 00:02:26 +0530

    user edit updated.

--- Loogan.Web.UI/wwwroot/js/user.js
+++ Loogan.Web.UI/wwwroot/js/user.js
@@ -107,21 +107,38 @@ function showUsers() {
             url: "/User/GetAllUser",
             data: {},
             success: function (data) {
+                let index = 1;
+                $('#tdBody').empty();
                 for (var item of data) {
                     $('#tdBody').append(`<tr>
-                        <td>${item.firstName}</td>
-                        <td>${item.lastName}</td>
-                        <td>${item.userName}</td>
-                        <td>${item.phoneNumber == null ? "" : item.phoneNumber}</td>
-                        <td>${item.emailAddress == null ? "" : item.emailAddress}</td>
-                        <td>${item.city == null ? "" : item.city}</td>
-                        <td>${item.state == null ? "" : item.state}</td>
-                        <td>${item.country == null ? "" : item.country}</td>
+                        <td><a id="f${index}" href="#" onclick="return userEdit(${index})" style="cursor:pointer">${item.firstName}</a></td>
+                        <td id="l${index}">${item.lastName}</td>
+                        <td id="u${index}">${item.userName}</td>
+                        <td id="ph${index}">${item.phoneNumber == null ? "" : item.phoneNumber}</td>
+                        <td id="e${index}">${item.emailAddress == null ? "" : item.emailAddress}</td>
+                        <td id="c${index}">${item.city == null ? "" : item.city}</td>
+                        <td id="s${index}">${item.state == null ? "" : item.state}</td>
+                        <td id="cou${index}">${item.country == null ? "" : item.country}</td>
+
+                        <td style="display:none" id="pa${index}">${item.password == null ? "" : item.password}</td>
+                        <td style="display:none" id="m${index}">${item.middleName == null ? "" : item.middleName}</td>
+                        <td style="display:none" id="a${index}">${item.additionalName == null ? "" : item.additionalName}</td>
+                        <td style="display:none" id="g${index}">${item.genderId == null ? "" : item.genderId}</td>
+                        <td style="display:none" id="pr${index}">${item.preFix == null ? "" : item.preFix}</td>
+                        <td style="display:none" id="su${index}">${item.suffix == null ? "" : item.suffix}</td>
+                        <td style="display:none" id="ed${index}">${item.educationLevel == null ? "" : item.educationLevel}</td>
+                        <td style="display:none" id="w${index}">${item.webSite == null ? "" : item.webSite}</td>
+
+                        <td style="display:none" id="fa${index}">${item.fax == null ? "" : item.fax}</td>
+                        <td style="display:none" id="com${index}">${item.company == null ? "" : item.company}</td>
+                        <td style="display:none" id="jo${index}">${item.jobTitle == null ? "" : item.jobTitle}</td>
+                        <td style="display:none" id="de${index}">${item.department == null ? "" : item.department}</td>
                     </tr>`)
+                    index++;
                 }
             },
             error: function (e) {
-
+                console.log(e);
             }
         })
 
@@ -135,14 +152,40 @@ function getUserEmailByUserName() {
         url: "/User/GetUserEmailByUserName",
         data: { userName: loginUserName },
         success: function (e) {
-
+            console.log('success');
         },
         error: function (e) {
-
+            console.log(e);
         }
     })
 }
 
 function sentMessage(msg) {
     Alert(msg, 'Success');
+}
+
+function userEdit(index) {
+    $('#txtFirstName').val($('#f' + index).html());
+    $('#txtMiddleName').val($('#m' + index).html());
+    $('#txtLastName').val($('#l' + index).html());
+    $('#txtAdditionalName').val($('#a' + index).html());
+    $('#txtEmailAddress').val($('#e' + index).html());
+    $('#txtUserName').val($('#u' + index).html());
+    $('#txtPassword').val($('#pa' + index).html());
+    $('#txtcnfrmpassword').val($('#pa' + index).html());
+    $('#ddlGender').val($('#g' + index).html());
+    $('#txtPreFix').val($('#pr' + index).html());
+    $('#txtSuffix').val($('#su' + index).html());
+    $('#ddlEductionLevel').val($('#ed' + index).html());
+    $('#txtWebsite').val($('#w' + index).html());
+    $('#txtPhone').val($('#ph' + index).html());
+    $('#txtCity').val($('#c' + index).html());
+    $('#txtState').val($('#s' + index).html());
+    $('#txtCountry').val($('#cou' + index).html());
+    $('#txtFaxNumber').val($('#fa' + index).html());
+    $('#txtCompany').val($('#com' + index).html());
+    $('#txtJobTitle').val($('#jo' + index).html());
+    $('#txtDepartment').val($('#de' + index).html());
+    $('#btnCreateClose').trigger('click');
+    return false;
 }
