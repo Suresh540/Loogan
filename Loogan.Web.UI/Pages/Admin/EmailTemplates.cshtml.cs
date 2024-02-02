ab97b86512229d70d357cba46b696457ac9b3e8e
Suresh Kalaga <suresh.kalaga@outlook.com>
2024-02-02 15:02:44 +0530

    Email template is completed

---
+++ Loogan.Web.UI/Pages/Admin/EmailTemplates.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Loogan.Web.UI.Pages.Admin
+{
+    public class EmailTemplatesModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
