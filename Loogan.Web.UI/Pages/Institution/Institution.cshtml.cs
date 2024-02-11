19d9b08035c784ca3ab784221e1b87bab405a3b1
Suresh Kalaga <suresh.kalaga@outlook.com>
2024-02-11 19:55:59 +0530

    Institution page implemented.

---
+++ Loogan.Web.UI/Pages/Institution/Institution.cshtml.cs
@@ -0,0 +1,38 @@
+using Loogan.API.Models.Models;
+using Loogan.API.Models.Models.Admin;
+using Loogan.Web.UI.Models;
+using Loogan.Web.UI.Pages.Shared;
+using Loogan.Web.UI.Resources.Pages;
+using Loogan.Web.UI.Utilities;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Localization;
+
+namespace Loogan.Web.UI.Pages.Institution
+{
+    public class InstitutionModel : PageModel 
+    {
+        private readonly IUtilityHelper _utilityHelper;
+
+        [BindProperty]
+        public List<InstitutionModelExtended>? Institutionlist { get; set; }
+
+        [BindProperty]
+        public List<InstitutionNewsModel>? InstituteNews { get; set; }
+
+        [BindProperty]
+        public List<InstitutionAnnouncementModel>? InstituteAnouncements { get; set; }
+
+        public InstitutionModel(IUtilityHelper utilityHelper)
+        {
+            _utilityHelper = utilityHelper;
+        }
+        public async Task OnGetAsync()
+        {
+            Institutionlist = await _utilityHelper.ExecuteAPICall<List<InstitutionModelExtended>>(null, RestSharp.Method.Post, resource: "api/Admin/GetAllInstitutions");
+            InstituteNews = await _utilityHelper.ExecuteAPICall<List<InstitutionNewsModel>>(null, RestSharp.Method.Post, resource: "api/Admin/GetAllInstitutionNews");
+            InstituteAnouncements = await _utilityHelper.ExecuteAPICall<List<InstitutionAnnouncementModel>>(null, RestSharp.Method.Post, resource: "api/Admin/GetAllInstitutionAnnouncement");
+        }
+    }
+}
