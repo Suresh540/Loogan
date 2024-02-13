e153cf019224590870b5f0d73be95220d4fc748b
Suresh Kalaga <suresh.kalaga@outlook.com>
2024-02-13 22:43:49 +0530

    institue page added.

--- Loogan.Web.UI/Pages/Institution/Institution.cshtml.cs
+++ Loogan.Web.UI/Pages/Institution/Institution.cshtml.cs
@@ -1,3 +1,4 @@
+using Loogan.API.Database.Models;
 using Loogan.API.Models.Models;
 using Loogan.API.Models.Models.Admin;
 using Loogan.Web.UI.Models;
@@ -5,6 +6,7 @@ using Loogan.Web.UI.Pages.Shared;
 using Loogan.Web.UI.Resources.Pages;
 using Loogan.Web.UI.Utilities;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Localization;
@@ -30,9 +32,10 @@ namespace Loogan.Web.UI.Pages.Institution
         }
         public async Task OnGetAsync()
         {
-            Institutionlist = await _utilityHelper.ExecuteAPICall<List<InstitutionModelExtended>>(null, RestSharp.Method.Post, resource: "api/Admin/GetAllInstitutions");
-            InstituteNews = await _utilityHelper.ExecuteAPICall<List<InstitutionNewsModel>>(null, RestSharp.Method.Post, resource: "api/Admin/GetAllInstitutionNews");
-            InstituteAnouncements = await _utilityHelper.ExecuteAPICall<List<InstitutionAnnouncementModel>>(null, RestSharp.Method.Post, resource: "api/Admin/GetAllInstitutionAnnouncement");
+            var apiRequest = new ApiRequest() { RequestValue = HttpContext.Session.GetInt32("LoginUserId").ToString() };
+            Institutionlist = await _utilityHelper.ExecuteAPICall<List<InstitutionModelExtended>>(apiRequest, RestSharp.Method.Post, resource: "api/Admin/GetAllInstitutions");
+            InstituteNews = await _utilityHelper.ExecuteAPICall<List<InstitutionNewsModel>>(apiRequest, RestSharp.Method.Post, resource: "api/Admin/GetAllInstitutionNews");
+            InstituteAnouncements = await _utilityHelper.ExecuteAPICall<List<InstitutionAnnouncementModel>>(apiRequest, RestSharp.Method.Post, resource: "api/Admin/GetAllInstitutionAnnouncement");
         }
     }
 }
