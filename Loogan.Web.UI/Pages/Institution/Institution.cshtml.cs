e6503a659c0aa83716949d1e8cb62fff4d814355
Suresh Kalaga <suresh.kalaga@outlook.com>
2024-02-16 16:33:19 +0530

    institue page completed

--- Loogan.Web.UI/Pages/Institution/Institution.cshtml.cs
+++ Loogan.Web.UI/Pages/Institution/Institution.cshtml.cs
@@ -4,10 +4,11 @@ using Loogan.Web.UI.Models;
 using Loogan.Web.UI.Utilities;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Globalization;
 
 namespace Loogan.Web.UI.Pages.Institution
 {
-    public class InstitutionModel : PageModel 
+    public class InstitutionModel : PageModel
     {
         private readonly IUtilityHelper _utilityHelper;
 
@@ -40,13 +41,31 @@ namespace Loogan.Web.UI.Pages.Institution
         private async Task GetAnnouncements(DateTime today, ApiRequest apiRequest)
         {
             InstituteAnouncements = await _utilityHelper.ExecuteAPICall<List<InstitutionAnnouncementModel>>(apiRequest, RestSharp.Method.Post, resource: "api/Admin/GetAllInstitutionAnnouncement");
-            InstituteAnouncements = InstituteAnouncements?.Where(x => Convert.ToDateTime(x.StartDate) >= today && Convert.ToDateTime(x.EndDate) <= today).Select(x => x).ToList();
+            InstituteAnouncements = InstituteAnouncements?.Where(x => GetDate(x.StartDate,x.EndDate)).Select(x => x).ToList();
         }
 
         private async Task GetNews(DateTime today, ApiRequest apiRequest)
         {
             InstituteNews = await _utilityHelper.ExecuteAPICall<List<InstitutionNewsModel>>(apiRequest, RestSharp.Method.Post, resource: "api/Admin/GetAllInstitutionNews");
-            InstituteNews = InstituteNews?.Where(x => Convert.ToDateTime(x.StartDate) >= today && Convert.ToDateTime(x.EndDate) <= today).Select(x => x).ToList();
+            InstituteNews = InstituteNews?.Where(x => GetDate(x.StartDate, x.EndDate)).Select(x => x).ToList();
+        }
+
+        private bool GetDate(string? startDateString, string? endDateString)
+        {
+            if (string.IsNullOrEmpty(startDateString) || string.IsNullOrEmpty(endDateString))
+            {
+                return true;
+            }
+            DateTime.TryParseExact(startDateString, "yyyy-MM-dd", CultureInfo.CurrentCulture, DateTimeStyles.NoCurrentDateDefault, out var startDate);
+            DateTime.TryParseExact(endDateString, "yyyy-MM-dd", CultureInfo.CurrentCulture, DateTimeStyles.NoCurrentDateDefault, out var endDate);
+            if(DateTime.Now.Date >= startDate && DateTime.Now.Date <= endDate)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
         }
     }
 }
